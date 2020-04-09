386f7049541e49d33f7ded4b1e494414ef87337f
rpdecks <rpdecks@gmail.com>
2020-04-09 13:34:41 -0400

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,7 +20,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT title, (SUM(amount) - funding_goal) AS amount_over 
+  "SELECT project.title, (SUM(amount) - funding_goal) AS amount_over 
   FROM projects JOIN pledges 
   ON projects.id = pledges.project_id
   GROUP BY projects.id
